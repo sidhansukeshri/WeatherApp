82f62df6e59dd85ed9822b0be65f05864cf2edb2
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2023-09-10 15:54:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,53 +1,181 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = 'eacea25a7a4fb172c2f8ad104be734ed'; // Replace with your OpenWeatherMap API key
-    const cityInput = document.getElementById('cityInput');
-    const searchButton = document.getElementById('searchButton');
-    const locationName = document.getElementById('locationName');
-    const weatherIcon = document.getElementById('weatherIcon');
-    const temperature = document.getElementById('temperature');
-    const weatherCondition = document.getElementById('weatherCondition');
-    const errorMessage = document.getElementById('errorMessage');
+/* Reset some default styles */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
 
-    searchButton.addEventListener('click', () => {
-        const city = cityInput.value;
+/* Set a background color for the body */
+body {
+    font-family: Arial, sans-serif;
+    background-color: #3498db; /* Attractive blue background color */
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('default-background.jpg'); /* Default background image */
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    transition: background-image 0.5s; /* Smooth background image transition */
+}
 
-        // Clear previous error message
-        errorMessage.textContent = '';
+/* Style the container for your content */
+.container {
+    max-width: 400px;
+    background-color: rgba(255, 255, 255, 0.95); /* Slightly transparent white background */
+    border-radius: 10px; /* Increased border radius for a modern look */
+    padding: 20px;
+    text-align: center;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Slight shadow for depth */
+}
 
-        if (city) {
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json(); // Parse the JSON response
-                })
-                .then(data => {
-                    if (data.cod === 200) {
-                        locationName.textContent = data.name;
-                        temperature.textContent = `${Math.round(data.main.temp - 273.15)}°C`;
-                        weatherCondition.textContent = data.weather[0].description;
-                        const iconCode = data.weather[0].icon;
-                        weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconCode}.png" alt="${data.weather[0].description}">`;
-                    } else {
-                        errorMessage.textContent = 'City not found';
-                        clearWeatherInfo();
-                    }
-                })
-                .catch(error => {
-                    errorMessage.textContent = 'An error occurred. Please try again later.';
-                    clearWeatherInfo();
-                });
-        } else {
-            errorMessage.textContent = 'Please enter a city name.';
-            clearWeatherInfo();
-        }
-    });
+/* Style the header */
+.header {
+    color: #333;
+    font-size: 36px;
+    margin-bottom: 20px;
+}
 
-    function clearWeatherInfo() {
-        locationName.textContent = '';
-        temperature.textContent = '';
-        weatherCondition.textContent = '';
-        weatherIcon.innerHTML = '';
+/* Style the input container */
+.input-container {
+    margin-top: 20px;
+}
+
+/* Style the text input */
+input[type="text"] {
+    padding: 10px;
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+    outline: none; /* Remove input field outline on focus */
+}
+
+/* Style the search button */
+button {
+    padding: 10px 20px;
+    background-color: #007BFF; /* Eye-catching blue button */
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    margin-top: 10px;
+    transition: background-color 0.3s; /* Smooth button color transition */
+}
+
+/* Change button color on hover */
+button:hover {
+    background-color: #0056b3; /* Darker blue on hover */
+    transform: scale(1.05); /* Scale up the button slightly on hover */
+}
+
+/* Style the weather information container */
+.weather-info {
+    margin-top: 20px;
+}
+
+/* Style the weather icon */
+.weather-icon {
+    font-size: 60px; /* Larger weather icon for emphasis */
+    color: #333; /* Change the color to match your design */
+}
+
+/* Style the location name */
+.location {
+    font-size: 24px;
+    font-weight: bold; /* Bold location name */
+    margin-top: 10px;
+}
+
+/* Style the temperature */
+.temperature {
+    font-size: 20px;
+    margin-top: 10px;
+}
+
+/* Style the weather condition */
+.weather-condition {
+    font-size: 16px;
+    margin-top: 10px;
+}
+
+/* Style additional weather details */
+.weather-detail {
+    font-size: 14px;
+    margin-top: 5px;
+}
+
+/* Style unit conversion toggle */
+.unit-toggle {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+}
+
+.unit-toggle label {
+    margin-right: 20px; /* Increase the margin-right to add more space */
+    color: #333;
+    font-weight: bold;
+}
+
+/* Add margin to the Fahrenheit label */
+.unit-toggle label:last-child {
+    margin-right: 0; /* No margin on the last label (Fahrenheit) */
+}
+
+
+
+/* Style error messages */
+.error-message {
+    color: red;
+    font-weight: bold;
+    margin-top: 10px;
+    font-size: 16px;
+}
+
+/* Add animation to the search button */
+button {
+    /* Existing button styles */
+    transition: background-color 0.3s, transform 0.2s ease-out;
+}
+
+button:hover {
+    background-color: #0056b3; /* Darker blue on hover */
+    transform: scale(1.05); /* Scale up the button slightly on hover */
+}
+
+/* Add fade-in animation for weather results */
+.weather-info {
+    opacity: 0;
+    animation: fadeIn 1s ease-in-out forwards;
+}
+
+@keyframes fadeIn {
+    0% {
+        opacity: 0;
     }
-});
+    100% {
+        opacity: 1;
+    }
+}
+
+/* Add color change animation for temperature and weather condition text */
+.temperature,
+.weather-condition {
+    color: transparent;
+    background-image: linear-gradient(45deg, #3498db, #007BFF);
+    background-clip: text;
+    -webkit-background-clip: text;
+    animation: colorChange 1s ease-in-out forwards;
+}
+
+@keyframes colorChange {
+    0% {
+        color: transparent;
+    }
+    100% {
+        color: #333; /* Change the color to match your design */
+    }
+}

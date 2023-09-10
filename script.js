139a81fa81b6d3dfb314f6048f4476f52b3547db
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2023-09-10 15:54:40 +0530

    script.js

--- script.js
+++ script.js
@@ -1,181 +1,101 @@
-/* Reset some default styles */
-* {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = 'eacea25a7a4fb172c2f8ad104be734ed'; // Replace with your OpenWeatherMap API key
+    const cityInput = document.getElementById('cityInput');
+    const searchButton = document.getElementById('searchButton');
+    const locationName = document.getElementById('locationName');
+    const weatherIcon = document.getElementById('weatherIcon');
+    const temperature = document.getElementById('temperature');
+    const weatherCondition = document.getElementById('weatherCondition');
+    const humidity = document.getElementById('humidity');
+    const windSpeed = document.getElementById('windSpeed');
+    const visibility = document.getElementById('visibility');
+    const unitToggle = document.getElementById('unitToggle');
+    const errorMessage = document.getElementById('errorMessage');
+    const backgroundImages = {
+        '01d': 'clear-sky-day.jpg',
+        '01n': 'clear-sky-night.jpg',
+        '02d': 'few-clouds-day.jpg',
+        '02n': 'few-clouds-night.jpg',
+        '03d': 'scattered-clouds.jpg',
+        '03n': 'scattered-clouds.jpg',
+        '04d': 'broken-clouds.jpg',
+        '04n': 'broken-clouds.jpg',
+        '09d': 'shower-rain-day.jpg',
+        '09n': 'shower-rain-night.jpg',
+        '10d': 'rain-day.jpg',
+        '10n': 'rain-night.jpg',
+        '11d': 'thunderstorm-day.jpg',
+        '11n': 'thunderstorm-night.jpg',
+        '13d': 'snow-day.jpg',
+        '13n': 'snow-night.jpg',
+        '50d': 'mist-day.jpg',
+        '50n': 'mist-night.jpg',
+    };
 
-/* Set a background color for the body */
-body {
-    font-family: Arial, sans-serif;
-    background-color: #3498db; /* Attractive blue background color */
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-image: url('default-background.jpg'); /* Default background image */
-    background-size: cover;
-    background-position: center;
-    background-repeat: no-repeat;
-    transition: background-image 0.5s; /* Smooth background image transition */
-}
+    unitToggle.addEventListener('change', () => {
+        // Handle unit conversion when the toggle changes
+        const unit = unitToggle.querySelector('input[name="unit"]:checked').value;
 
-/* Style the container for your content */
-.container {
-    max-width: 400px;
-    background-color: rgba(255, 255, 255, 0.95); /* Slightly transparent white background */
-    border-radius: 10px; /* Increased border radius for a modern look */
-    padding: 20px;
-    text-align: center;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); /* Slight shadow for depth */
-}
+        // Check if temperature has a value and update it with the selected unit
+        if (temperature.textContent) {
+            const currentTemp = parseFloat(temperature.textContent);
+            const convertedTemp = unit === 'metric' ? (currentTemp - 32) * 5/9 : (currentTemp * 9/5) + 32;
+            temperature.textContent = `${convertedTemp.toFixed(1)}°${unit === 'metric' ? 'C' : 'F'}`;
+        }
+    });
 
-/* Style the header */
-.header {
-    color: #333;
-    font-size: 36px;
-    margin-bottom: 20px;
-}
+    searchButton.addEventListener('click', () => {
+        const city = cityInput.value;
+        const unit = unitToggle.querySelector('input[name="unit"]:checked').value;
 
-/* Style the input container */
-.input-container {
-    margin-top: 20px;
-}
+        // Clear previous error message
+        errorMessage.textContent = '';
 
-/* Style the text input */
-input[type="text"] {
-    padding: 10px;
-    width: 100%;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    font-size: 16px;
-    outline: none; /* Remove input field outline on focus */
-}
+        if (city) {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`)
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.json(); // Parse the JSON response
+                })
+                .then(data => {
+                    if (data.cod === 200) {
+                        locationName.textContent = data.name;
+                        temperature.textContent = `${data.main.temp.toFixed(1)}°${unit === 'metric' ? 'C' : 'F'}`;
+                        weatherCondition.textContent = data.weather[0].description;
+                        humidity.textContent = `Humidity: ${data.main.humidity}%`;
+                        windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+                        visibility.textContent = `Visibility: ${data.visibility} meters`;
+                        const iconCode = data.weather[0].icon;
+                        weatherIcon.innerHTML = `<img src="http://openweathermap.org/img/wn/${iconCode}.png" alt="${data.weather[0].description}">`;
+                        // Change background image based on weather condition
+                        const background = backgroundImages[iconCode] || 'default-background.jpg';
+                        document.body.style.backgroundImage = `url('${background}')`;
+                    } else {
+                        errorMessage.textContent = 'City not found';
+                        clearWeatherInfo();
+                    }
+                })
+                .catch(error => {
+                    errorMessage.textContent = 'An error occurred. Please try again later.';
+                    clearWeatherInfo();
+                });
+        } else {
+            errorMessage.textContent = 'Please enter a city name.';
+            clearWeatherInfo();
+        }
+    });
 
-/* Style the search button */
-button {
-    padding: 10px 20px;
-    background-color: #007BFF; /* Eye-catching blue button */
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    font-size: 16px;
-    margin-top: 10px;
-    transition: background-color 0.3s; /* Smooth button color transition */
-}
-
-/* Change button color on hover */
-button:hover {
-    background-color: #0056b3; /* Darker blue on hover */
-    transform: scale(1.05); /* Scale up the button slightly on hover */
-}
-
-/* Style the weather information container */
-.weather-info {
-    margin-top: 20px;
-}
-
-/* Style the weather icon */
-.weather-icon {
-    font-size: 60px; /* Larger weather icon for emphasis */
-    color: #333; /* Change the color to match your design */
-}
-
-/* Style the location name */
-.location {
-    font-size: 24px;
-    font-weight: bold; /* Bold location name */
-    margin-top: 10px;
-}
-
-/* Style the temperature */
-.temperature {
-    font-size: 20px;
-    margin-top: 10px;
-}
-
-/* Style the weather condition */
-.weather-condition {
-    font-size: 16px;
-    margin-top: 10px;
-}
-
-/* Style additional weather details */
-.weather-detail {
-    font-size: 14px;
-    margin-top: 5px;
-}
-
-/* Style unit conversion toggle */
-.unit-toggle {
-    display: flex;
-    justify-content: center;
-    margin-top: 20px;
-}
-
-.unit-toggle label {
-    margin-right: 20px; /* Increase the margin-right to add more space */
-    color: #333;
-    font-weight: bold;
-}
-
-/* Add margin to the Fahrenheit label */
-.unit-toggle label:last-child {
-    margin-right: 0; /* No margin on the last label (Fahrenheit) */
-}
-
-
-
-/* Style error messages */
-.error-message {
-    color: red;
-    font-weight: bold;
-    margin-top: 10px;
-    font-size: 16px;
-}
-
-/* Add animation to the search button */
-button {
-    /* Existing button styles */
-    transition: background-color 0.3s, transform 0.2s ease-out;
-}
-
-button:hover {
-    background-color: #0056b3; /* Darker blue on hover */
-    transform: scale(1.05); /* Scale up the button slightly on hover */
-}
-
-/* Add fade-in animation for weather results */
-.weather-info {
-    opacity: 0;
-    animation: fadeIn 1s ease-in-out forwards;
-}
-
-@keyframes fadeIn {
-    0% {
-        opacity: 0;
+    function clearWeatherInfo() {
+        locationName.textContent = '';
+        temperature.textContent = '';
+        weatherCondition.textContent = '';
+        humidity.textContent = '';
+        windSpeed.textContent = '';
+        visibility.textContent = '';
+        weatherIcon.innerHTML = '';
+        // Reset background to default
+        document.body.style.backgroundImage = `url('default-background.jpg')`;
     }
-    100% {
-        opacity: 1;
-    }
-}
-
-/* Add color change animation for temperature and weather condition text */
-.temperature,
-.weather-condition {
-    color: transparent;
-    background-image: linear-gradient(45deg, #3498db, #007BFF);
-    background-clip: text;
-    -webkit-background-clip: text;
-    animation: colorChange 1s ease-in-out forwards;
-}
-
-@keyframes colorChange {
-    0% {
-        color: transparent;
-    }
-    100% {
-        color: #333; /* Change the color to match your design */
-    }
-}
+});
